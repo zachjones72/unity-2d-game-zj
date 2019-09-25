3365cc02ecb581b1ad13ede33cd488a02cd4aa57
zachjones72 <55810574+zachjones72@users.noreply.github.com>
2019-09-25 15:38:38 -0500

    added

---
+++ Worm/Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public static CollectibleManager Instance;
+
+    private int _totalCoins;
+    private int _collectedCoins;
+
+    public TextMeshProUGUI coinCount;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        _totalCoins = FindObjectsOfType<Collectible>().Length;
+        DisplayCoinCount();
+    }
+    void DisplayCoinCount()
+    {
+        coinCount.SetText(_collectedCoins + " / " + _totalCoins);
+
+        if(_collectedCoins == _totalCoins)
+        {
+            coinCount.SetText("You win?");
+        }
+    }
+
+    public void HandleCoinCollected()
+    {
+        _collectedCoins++;
+        DisplayCoinCount();
+    }
+}
