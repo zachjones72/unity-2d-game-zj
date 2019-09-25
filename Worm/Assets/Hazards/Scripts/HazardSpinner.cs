a26c7f78bcf771f19decda0e3c3b75676c6fdb1d
zachjones72 <55810574+zachjones72@users.noreply.github.com>
2019-09-25 16:57:06 -0500

    hi

---
+++ Worm/Assets/Hazards/Scripts/HazardSpinner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HazardSpinner : MonoBehaviour
+{
+    public Transform pivot;
+
+    [Range(0,15)]
+    public float speed;
+
+    public bool isClockwise;
+    private void Update()
+    {
+        if(isClockwise)
+        {
+            pivot.Rotate(Vector3.back * speed);
+        }
+        else
+        {
+            pivot.Rotate(Vector3.forward * speed);
+        }
+    }
+}
